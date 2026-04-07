1a069248a4dca8a5e860153e0abe5bd6108681e7
cloud25-web <drishti.ii1910@gmail.com>
2026-04-07 14:16:41 +0530

    classwork

---
+++ javascript/js basics/var.js
@@ -0,0 +1,38 @@
+/*var name= "John doe";
+var age= 30;
+var isEmployed= true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+var age=50;
+console.log(age);*/
+let name= "drishti singh";
+var isEmployed= true;
+//let age = 20;
+console.log(name);
+console.log(isEmployed);
+//console.log(age);
+//let age= 30;
+//console.log(age);
+/*if(true){
+  var age= 30;
+}
+console.log(age);*/
+/*if(true){
+  let age= 30;
+}
+console.log(age);*/
+const PI= 3.14;
+console.log(PI);
+
+console.log(typeof name);
+console.log(typeof isEmployed);
+
+let name1 = "Drishti";
+let age1 = 20;
+console.log("My name is " + name1 + " and my age is " + age1); //concatenation
+console.log(`My name is ${name1} and age is ${age1}`);
+
+/*let result;
+console.log(result);
+console.log(typeOf result);*/
